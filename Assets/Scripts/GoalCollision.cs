e8dc9ce8c13105d26e1b68c146ec4c1da384890d
Cameron Penner <cameronp@iqmetrix.com>
2015-03-06 17:27:56 -0600

    doing stuff

---
+++ Assets/Scripts/GoalCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("found end");
+            col.gameObject.rigidbody.AddForce(new Vector3(0,5,0));
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed;
     public float MaxSpeed;
+    private Vector3 _motion;
+    public float PrevX;
+    public float PrevZ;
+
     private Vector3 _input;
 
 	// Use this for initialization
@@ -13,12 +18,45 @@ public class PlayerMovement : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update ()
-	{
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-	    if (rigidbody.velocity.magnitude < MaxSpeed)
-	    {
-	        rigidbody.AddForce(_input*MoveSpeed);
-	    }
-	}
+        PrevX = rigidbody.position.x;
+        PrevZ = rigidbody.position.z;
+
+        if (Mathf.Abs(_motion.x) > 0)
+            _motion.x -= MoveSpeed*Mathf.Sign(_motion.x);
+        if (_motion.x < MoveSpeed)
+            _motion.x = 0;
+
+        if (Mathf.Abs(_motion.z) > 0)
+            _motion.z -= MoveSpeed * Mathf.Sign(_motion.z);
+        if (_motion.z < MoveSpeed)
+            _motion.z = 0;
+
+
+        _motion += _input*MoveSpeed;
+
+        if (_motion.x > MaxSpeed)
+            _motion.x = MaxSpeed;
+        if (_motion.x < -MaxSpeed)
+            _motion.x = -MaxSpeed;
+
+        if (_motion.z > MaxSpeed)
+            _motion.z = MaxSpeed;
+        if (_motion.z < -MaxSpeed)
+            _motion.z = -MaxSpeed;
+
+        rigidbody.position += _motion;
+    }
+
+    public void FindGoal()
+    {
+        return;
+    }
 }
