943b63b1488dd0101185636fd1d47fab4676084f
Cameron Penner <cameronp@iqmetrix.com>
2015-03-06 14:03:30 -0600

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float MaxSpeed;
+    private Vector3 _input;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        _input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	    if (rigidbody.velocity.magnitude < MaxSpeed)
+	    {
+	        rigidbody.AddForce(_input*MoveSpeed);
+	    }
+	}
+}
