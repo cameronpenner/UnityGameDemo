d649eaaa64975ed7a695e7f4a79c1b2975120a43
Cameron Penner <cameronp@iqmetrix.com>
2015-03-18 17:28:12 -0500

    stuffs

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bullet")
+        {
+            Debug.Log("OH SNAP"+col.transform);
+            Destroy(gameObject);
+        }
+    }
+}
